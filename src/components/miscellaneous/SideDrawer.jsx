6de11900a280ff20261be6d9198d6562524fea80
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-08-08 18:40:12 +0530

    Final Push

--- src/components/miscellaneous/SideDrawer.jsx
+++ src/components/miscellaneous/SideDrawer.jsx
@@ -134,7 +134,7 @@ function SideDrawer() {
         borderWidth="5px"
       >
         <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
-          <Button variant="ghost" onClick={onOpen}>
+          <Button variant="solid" colorScheme='cyan' onClick={onOpen}>
             <i className="fas fa-search"></i>
             <Text d={{ base: "none", md: "flex" }} px={4}>
               Search User
